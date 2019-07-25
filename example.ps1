9ba55993e6d5be089608d28e7b73b2ff85f911df
Hubert <wieland.hubert@gmx.at>
2019-07-25 18:55:31 +0200

    modified PowerShellScript
      -added patientId csv example

--- example.ps1
+++ example.ps1
@@ -3,8 +3,20 @@
 
 #Upload filter - uploads all files with specific file extension
 function shouldUploadFile {
-  param($path)
-  $whitelist = @("jpg", "jpeg", "png", "bmp")
-  $fileExtension = [System.IO.Path]::GetExtension($path).TrimStart(".")
-  return $whitelist.Contains($fileExtension)
+    param($path)
+    $whitelist = @("jpg", "jpeg", "png", "bmp")
+    $fileExtension = [System.IO.Path]::GetExtension($path).TrimStart(".")
+    return $whitelist.Contains($fileExtension)
+}
+
+#Extracts patientId from csv - File (input parameter: file path)
+function getPatientIdFromPath {
+    param($path)
+    $folderPath = Split-Path $path -Parent
+    $csvFile = Get-ChildItem $folderPath | where {$_.extension -eq ".csv"} | select -ExpandProperty Fullname
+    $columnName = "personId"
+    #single row -> returns string
+    $csvContent = (Get-Content $csvFile  -Encoding:string | convertfrom-csv -delimiter ",").$columnName
+    $patientId = $csvContent -as [int]
+    return $patientId
 }
